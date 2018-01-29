2032465fb46d6475efdb96f4e4e3518198ca340f
Peter McCready <petermccready2@gmail.com>
2018-01-29 21:45:48 +0000

    amends data structure

--- src/data.js
+++ src/data.js
@@ -1,68 +1,154 @@
 const data = [
   {
-    id: 1,
-    firstName: 'Gerry',
-    secondName: 'McCready',
-    lead: true,
-    sport: 'Ski',
-    paid: true,
-    flightId: 1,
-    transferId: 1,
-    accomodationId: 1,
-    liftPassId: 1,
-    equipmentId: 1
-  },
-  {
-    id: 2,
-    firstName: 'Michael',
-    secondName: 'McCready',
-    lead: false,
-    sport: 'Ski',
-    paid: true,
-    flightId: 2,
-    transferId: 2,
-    accomodationId: 2,
-    liftPassId: 2,
-    equipmentId: 2
-  },
-  {
-    id: 3,
-    firstName: 'Michael',
-    secondName: 'McCready',
-    lead: false,
-    sport: 'Snowboard',
-    paid: true,
-    flightId: 3,
-    transferId: 3,
-    accomodationId: 3,
-    liftPassId: 3,
-    equipmentId: 3
-  },
-  {
-    id: 4,
-    firstName: 'Eleanor',
-    secondName: 'McCready',
-    lead: false,
-    sport: 'Ski',
-    paid: false,
-    flightId: 4,
-    transferId: 4,
-    accomodationId: 4,
-    liftPassId: 4,
-    equipmentId: 4
-  },
-  {
-    id: 5,
     firstName: 'Maureen',
     secondName: 'McCready',
     lead: false,
     sport: '',
-    paid: false,
-    flightId: 5,
-    transferId: 5,
-    accomodationId: 5,
-    liftPassId: 5,
-    equipmentId: 5
+    outboundFlight: {
+      flightNumber: 'EZY6835',
+      departureAirport: {
+        name: 'Glasgow',
+        abbr: 'GLA',
+        country: 'Scotland, UK'
+      },
+      destinationAirport: {
+        name: 'Geneva',
+        abbr: 'GVA',
+        country: 'Switzerland'
+      },
+      depart: '18/03/2018 07:55',
+      arrive: '18/03/2018 11:20',
+      price: 40.00
+    },
+    returnFlight: {
+      flightNumber: 'EZY6836',
+      departureAirport: {
+        name: 'Geneva',
+        abbr: 'GVA',
+        country: 'Switzerland'
+      },
+      destinationAirport: {
+        name: 'Glasgow',
+        abbr: 'GLA',
+        country: 'Scotland, UK'
+      },
+      depart: '25/03/2018 12:00',
+      arrive: '25/03/2018 13:20',
+      price: 50.00
+    },
+    outboundTransfer: {
+      pickup: 'Geneva Airport, Switzerland',
+      dropOff: 'Chalet Bouquetins, 73210 La Perriere, La Tania, France',
+      depart: '18/03/2018 12:00',
+      arrive: '18/03/2018 15:30',
+      price: 0.00
+    },
+    returnTransfer: {
+      pickup: 'Chalet Bouquetins, 73210 La Perriere, La Tania, France',
+      dropOff: 'Geneva Airport, Switzerland',
+      depart: '25/03/2018 07:00',
+      arrive: '25/03/2018 10:30',
+      price: 0.00
+    },
+    accomodation: {
+      name: 'Chalet Bouquetins',
+      address: '73210 La Perriere, La Tania, France',
+      operator: 'SkiHame',
+      catered: true,
+      deposit: 100.00,
+      depositPaid: true,
+      price: 475.35,
+      totalPaid: false
+    },
+    liftPass: {
+      area: '',
+      duration: null,
+      insurance: false,
+      price: 0.00
+    },
+    equipment: {
+      hiring: false,
+      hireShop: '',
+      type: '',
+      size: null,
+      price: 0.00
+    }
+  },
+  {
+    firstName: 'Andrew',
+    secondName: 'McCready',
+    lead: false,
+    sport: 'Snowboard',
+    paid: true,
+    outboundFlight: {
+      flightNumber: 'EZY6835',
+      departureAirport: {
+        name: 'Glasgow',
+        abbr: 'GLA',
+        country: 'Scotland, UK'
+      },
+      destinationAirport: {
+        name: 'Geneva',
+        abbr: 'GVA',
+        country: 'Switzerland'
+      },
+      depart: '18/03/2018 07:55',
+      arrive: '18/03/2018 11:20',
+      price: 40.00
+    },
+    returnFlight: {
+      flightNumber: 'EZY6836',
+      departureAirport: {
+        name: 'Geneva',
+        abbr: 'GVA',
+        country: 'Switzerland'
+      },
+      destinationAirport: {
+        name: 'Glasgow',
+        abbr: 'GLA',
+        country: 'Scotland, UK'
+      },
+      depart: '25/03/2018 12:00',
+      arrive: '25/03/2018 13:20',
+      price: 50.00
+    },
+    outboundTransfer: {
+      pickup: 'Geneva Airport, Switzerland',
+      dropOff: 'Chalet Bouquetins, 73210 La Perriere, La Tania, France',
+      depart: '18/03/2018 12:00',
+      arrive: '18/03/2018 15:30',
+      price: 0.00
+    },
+    returnTransfer: {
+      pickup: 'Chalet Bouquetins, 73210 La Perriere, La Tania, France',
+      dropOff: 'Geneva Airport, Switzerland',
+      depart: '25/03/2018 07:00',
+      arrive: '25/03/2018 10:30',
+      price: 0.00
+    },
+    accomodation: {
+      name: 'Chalet Bouquetins',
+      address: '73210 La Perriere, La Tania, France',
+      operator: 'SkiHame',
+      catered: true,
+      deposit: 100.00,
+      depositPaid: true,
+      price: 475.35,
+      totalPaid: true
+    },
+    liftPass: {
+      area: 'Three Valleys',
+      duration: 6,
+      insurance: true,
+      price: 239.99
+    },
+    equipment: {
+      hiring: true,
+      hireShop: 'Freeride',
+      type: 'Snowboard',
+      size: 155,
+      price: 60.12
+    }
   }
 ]
 
