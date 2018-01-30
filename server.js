1b902d9986330935fa6e86488cfd2ae88b094e35
Peter McCready <petermccready2@gmail.com>
2018-01-30 17:20:52 +0000

    adds flight API route

--- server.js
+++ server.js
@@ -2,17 +2,33 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var ObjectId = require('mongodb').ObjectId;
+var MongoClient = require('mongodb').MongoClient;
 
 // instances
 var app = express();
 var router = express.Router();
 
-// set our port to either a predetermined port number if you have set it up, or 3001
+// set port to either a predetermined port number, or 3001
 var port = process.env.API_PORT || 3001;
 
 // configure the API to use bodyParser and look for JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// connect to mongodb
+MongoClient.connect('mongodb://localhost:27017/ski_hol_planner', function(err, client){
+  if (err){
+    return console.log(err);
+  }
+  db = client.db('ski_hol_planner');
+  console.log('Connected to DB');
+
+  // ensure app is listening on the port
+  app.listen(port, function(){
+    console.log(`App listening on port ${port}`);
+  });
+});
 
 // prevent Cross Origin Resource Sharing (CORS) errors, set headeres to allow CORS with middleware
 app.use(function(req, res, next){
@@ -26,12 +42,36 @@ app.use(function(req, res, next){
   next();
 });
 
+// routes
 router.get('/', function(req, res){
-  res.json({ message: 'API initialised!' });
+  res.sendFile(__dirname + '/public/index.html');
 });
 
+router.get('/members', function(req, res){
+  console.log('query: all party members');
+  db.collection('members').find().toArray(function(err, results){
+    if (err){ return console.log('DB error') };
+    res.json(results);
+  });
+});
+
+router.get('/members/:name', function(req, res){
+  var person = { $or: [ { firstName_lower: req.params.name.toLowerCase() }, { secondName_lower: req.params.name.toLowerCase() } ] };
+  console.log('query: ', person);
+  db.collection('members').find(person).toArray(function(err, results){
+    if (err){ return console.log()}
+    res.json(results);
+  });
+});
+
+router.get('/flight/:flightNumber', function(req, res){
+  var flight = { $or: [ { 'outboundFlight.flightNumber': req.params.flightNumber }, { 'returnFlight.flightNumber': req.params.flightNumber } ] };
+  console.log('query: ', flight);
+  db.collection('members').find(flight).toArray(function(err, results){
+    if (err){ return console.log('DB error') };
+    res.json(results);
+  });
+});
+
+// route startpoint and callback
 app.use('/api', router);
-
-app.listen(port, function(){
-  console.log(`App listening on port ${port}`);
-});
